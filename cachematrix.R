27914c9461626e13679011a4e6cf07384a518fb7
Binod Raj Pathak <bipathak@veriskhealth.com>
2016-03-16 19:04:05 +0545

    Changes for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  cacheInvMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    cacheInvMatrix <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) cacheInvMatrix <<- inverse
+  getInverse <- function() cacheInvMatrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+invFunc <- x$getInverse()
+  if(!is.null(invFunc)) {
+    message("<< cached data >>")
+    return(invFunc)
+  }
+  data <- x$get()
+  invFunc <- solve(data, ...)
+  x$setInverse(invFunc)
+  invFunc		
 }
